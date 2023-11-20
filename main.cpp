9fbf34756ab0d5b52b325668f20e3356d6de9efa
Ariana Argyrakis <116682744+arianaargyrakis@users.noreply.github.com>
2023-11-19 21:05:41 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Car.h"
+
+int main() {
+    Car car; //use to make car objects for each car in the data
+    //read data
+
+    PriorityQueue q;
+    Car ideal_car = q.getPriority();
+
+    //print stuff using ideal_car
+
+    return 0;
+}
