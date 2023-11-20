bd1519de83497b42d5d86763c03675748773c708
Ariana Argyrakis <116682744+arianaargyrakis@users.noreply.github.com>
2023-11-19 21:06:47 -0500

    Create Car.h

---
+++ Car.h
@@ -0,0 +1,44 @@
+#ifndef PROJECT_3_CAR_H
+#define PROJECT_3_CAR_H
+
+#endif //PROJECT_3_CAR_H
+
+#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Car {
+    //not sure if we'll use classification, make, model_year, or year, but I put them for now
+    string size;
+    string driveline;
+    string engine_type;
+    bool hybrid;
+    int num_of_forward_gears;
+    string transmission;
+    int city_mpg;
+    string fuel_type;
+    int highway_mpg;
+    string classification;
+    string ID;
+    string make;
+    string model_year;
+    string year;
+    int horsepower;
+    int torque;
+
+    string determineSize(int height, int length, int width) {
+        //gets the size for the size variable in the car class
+        //will be implemented later
+    }
+};
+
+class PriorityQueue {
+public:
+    vector<Car> priority_vect;
+
+    Car getPriority() {
+        //returns the car with the highest priority(the ideal car)
+        //will be implemented later
+    }
+};
