f4ab85e1ac9e9ca94e7443f7451b3ca4d8bbcb41
RustyShackleford007 <andrewgoodsonfl@gmail.com>
2023-12-02 15:37:30 -0500

    Priority Queue "q" filled with data and updated input for FWD and all different fuel tyes

--- main.cpp
+++ main.cpp
@@ -161,7 +161,6 @@ int main() {
     {
         idealCar.push_back(cityMPG);
         idealCar.push_back(highwayMPG);
-
         if (transmissionType == "1")
         {
             idealCar.push_back("automatic");
